b5c077bf67601d9496b1e76ec9a7900aff3ed0e0
egeakat <egeakat@hotmail.com>
2023-04-17 18:31:27 +0300

    added chatter's name color to messages

--- js/main.js
+++ js/main.js
@@ -10,7 +10,20 @@ window.onload = function () {
   client.connect();
 
   client.on("message", (channel, tags, message, self) => {
-    document.getElementById("chat").innerText += `${tags["username"]}: ${message}\n`;
+    let userNameSpan = document.createElement("span");
+    let messageSpan = document.createElement("span");
+    let brTag = document.createElement("br");
+
+    userNameSpan.style.color = tags["color"];
+    userNameSpan.innerHTML = tags["username"] + ": ";
+
+    messageSpan.innerHTML = message;
+
+    document.getElementById("chat").appendChild(userNameSpan);
+    document.getElementById("chat").appendChild(messageSpan);
+    document.getElementById("chat").appendChild(brTag);
+    
+    //document.getElementById("chat").innerText += `${tags["username"]}: ${message}\n`;
   });
 
   document.getElementById("save-chat").addEventListener("click", saveLast100Lines);
